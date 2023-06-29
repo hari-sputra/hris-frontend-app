31e2bacebb6509977225c851efca2ce7e3063ea3
hari saputra <hari.saputra.dev@gmail.com>
2023-06-29 15:37:41 +0700

    init project and add pages

---
+++ layouts/dashboard.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <Sidebar />
+        <nuxt />
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="py-[200px] flex flex-col items-center justify-center px-4">
+    <div class="text-[32px] font-semibold text-dark mb-4">Select Companies</div>
+    <form class="w-full card">
+      <div class="form-group">
+        <label class="text-grey">
+          Companies
+        </label>
+        <select class="appearance-none input-field form-icon-chevron-down">
+          <option value="" selected>Company Name</option>
+        </select>
+      </div>
+      <a href="/company/1" class="w-full btn btn-primary mt-[14px]">Continue</a>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
